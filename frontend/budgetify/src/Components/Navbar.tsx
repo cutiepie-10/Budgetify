834b113a71f764e4265d1ff629363d5e4109c8db
cutiepie-10 <jay.aroracs@gmail.com>
2025-06-17 13:28:36 +0530

    initial-commit

---
+++ frontend/budgetify/src/Components/Navbar.tsx
@@ -0,0 +1,10 @@
+const Navbar= ()=>{
+    return(
+        <div className="flex flex-col h-[50px] bg-green-400 sticky p-[5px] justify-around">
+            <div className="h-1">
+                Budgetify
+            </div>
+        </div>
+    );
+}
+export default Navbar;
--- frontend/src/Components/Welcome/contactUs.tsx
+++
@@ -1,8 +0,0 @@
-export default function ContactUs(){
-    return(
-        <div className="w-full relative overflow-clip  text-white bg-red-400 
-        flex flex-col gap-3 item-center ">
-            Conatct US
-        </div>
-    )
-}
